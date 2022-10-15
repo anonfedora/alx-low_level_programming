ced1415101d9c50bcabe3a6117b7c2bb1ba2e0f0
Eleazar <musaeleazar090@gmail.com>
2022-10-15 01:06:13 +0000

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+/**
+ * main - Entry point for program
+ *
+ * Description: 'Quote'
+ *
+ *
+ * Return: Always 1 (When success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+}
