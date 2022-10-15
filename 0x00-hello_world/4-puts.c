22df6803726da867d70cffe1129261e6e770a2d5
Eleazar <musaeleazar090@gmail.com>
2022-10-15 00:35:53 +0000

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point for program
+ *
+ * Description: 'C low programming'
+ *
+ *
+ * Return: Always 0 (When success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
