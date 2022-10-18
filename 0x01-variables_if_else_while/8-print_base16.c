259af9c3aac0f5bad77334b3ee69b605837aae72
Eleazar <musaeleazar090@gmail.com>
2022-10-18 01:48:37 +0000

    Print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * You can only use the putchar
+ * function (every other function (printf, puts, etc…) is forbidden)
+ * All your code should be in the main function
+ * You can only use putchar three times in your code
+ * Skip the print of  q and e
+ */
+
+/**
+ * main - Entry point of the program
+ *
+ *Return: 0 Always (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+
+	while (ch <= 'f')
+	{
+		putchar(ch);
+
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
+
